13fc73807bf8fa259921f1f5f84e64014f17533a
54398-worada <54398@yupparaj.ac.th>
2026-08-19 16:03:52 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,10 +1,11 @@
-  import streamlit as st
-   st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+import streamlit as st
+st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 
-  price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 
- vat = price * 0.07
-net_price = price - vat
-  st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+ net_price = price - vat
+
+  st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+
 st.divider()
 st.write(" นางสาววรดา พึ่งตน เลขที่ 14 ม.4/4")
