047785599394fe1909985c7dcdd63635ee80f74e
54398-worada <54398@yupparaj.ac.th>
2026-08-19 16:06:15 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
- st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
 
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 
