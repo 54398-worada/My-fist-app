6fa13ccfb017b5e9e7ebf152b085bcb346d43769
54398-worada <54398@yupparaj.ac.th>
2026-08-19 16:13:40 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,11 +1,7 @@
 import streamlit as st
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
-
-
- price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-
-
- vat = price * 0.07
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07
 
 
 net_price = price - vat
