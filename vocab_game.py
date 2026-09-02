dd344ff71d9726bbaa763474af457fbad5ad0adf
54398-worada <54398@yupparaj.ac.th>
2026-09-02 16:19:31 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -101,5 +101,5 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวดีใจ ยิ้มแย้ม เลขที่ 5 ม.4/5")
+st.write("นางวรดา พึ่งตน เลขที่14 ม.4/4")
 
